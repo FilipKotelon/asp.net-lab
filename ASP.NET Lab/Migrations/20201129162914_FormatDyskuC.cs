183d754f7e45988acf527cf4ec58318c2eaedb21
Blake Greenleaf <off.f.kotelon@gmail.com>
2020-11-29 17:33:08 +0100

    Lab4

---
+++ ASP.NET Lab/Migrations/20201129162914_FormatDyskuC.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASP.NET_Lab.Migrations
+{
+    public partial class FormatDyskuC : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ ASP.NET Lab/ViewComponents/LatestItem.cs
@@ -0,0 +1,27 @@
+﻿using ASP.NET_Lab.Database;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Lab.ViewComponents
+{
+    public class LatestItem : ViewComponent
+    {
+        private readonly ExchangesDbContext _dbContext;
+
+        public LatestItem(ExchangesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var latestItem = _dbContext.Items
+                .OrderByDescending(x => x.Id).First();
+
+            return View(latestItem);
+        }
+    }
+}
